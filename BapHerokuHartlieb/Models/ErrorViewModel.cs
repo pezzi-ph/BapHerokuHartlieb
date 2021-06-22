e34a183076a4a2f59361eec1f13ec6d7ff2df72d
Patrick Hartlieb <har18834@spengergasse.at>
2021-06-22 16:15:14 +0200

    dotnet project

---
+++ BapHerokuHartlieb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BapHerokuHartlieb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
